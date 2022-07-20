a9a9a947582f1800c324924df8e74b8fe5609b0a
LeAnnAlexandra <LAVTechInternational@gmail.com>
2022-07-20 09:10:35 +0200

    This is normal update as the program is being developed.

--- TestNetworkPrint/Form1.Designer.cs
+++ TestNetworkPrint/Form1.Designer.cs
@@ -28,20 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "WifiSniffer";
+            this.notifyIcon1.BalloonTipTitle = "WifiSniffer";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Wifi-Listener, Bitch";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openSettingsToolStripMenuItem,
+            this.closeExitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 48);
+            // 
+            // openSettingsToolStripMenuItem
+            // 
+            this.openSettingsToolStripMenuItem.Name = "openSettingsToolStripMenuItem";
+            this.openSettingsToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.openSettingsToolStripMenuItem.Text = "&Open &Settings";
+            this.openSettingsToolStripMenuItem.Click += new System.EventHandler(this.openSettingsToolStripMenuItem_Click);
+            // 
+            // closeExitToolStripMenuItem
+            // 
+            this.closeExitToolStripMenuItem.Name = "closeExitToolStripMenuItem";
+            this.closeExitToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.closeExitToolStripMenuItem.Text = "Close/E&xit";
+            this.closeExitToolStripMenuItem.Click += new System.EventHandler(this.closeExitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Settings";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Hide at Startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(3, 28);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(153, 19);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Install to Start-up Folder";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Adv. Options";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(3, 53);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 19);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Full Auto";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 45);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "By Clicking Okay, You Accept Terms and \r\nConditions and  Agree to Using The App o" +
+    "n \r\nThe Recommended Full-Auto Settings. ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Accept";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(3, 78);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(142, 19);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "Open on Double Click";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox2);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox3);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(25, 137);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 110);
+            this.flowLayoutPanel1.TabIndex = 10;
+            this.flowLayoutPanel1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(324, 151);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wifi Listener Settings";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem openSettingsToolStripMenuItem;
+        private ToolStripMenuItem closeExitToolStripMenuItem;
+        private Label label1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button button1;
+        private CheckBox checkBox3;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private CheckBox checkBox4;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- TestNetworkPrint/Form1.cs
+++ TestNetworkPrint/Form1.cs
@@ -1,21 +1,40 @@
 namespace TestNetworkPrint
 {
+    using System.Diagnostics;
     public partial class Form1 : Form
     {
-        private const bool hiddenGem = false;
+        /*
+        #####################################################################
+                                    Please Note:
+        The idea is that only have to edit the constants in order to see the differences. 
+        
+        For instance, during debut, HiddenGem = true (to show what it will look like in release. 
+        
+        ########################################################################
+        */
+        //private bool OpenOnDoubleClick = false;
+        private bool debug = true;
+        // private bool HiddenGem = true; // Please set this to true - false will automatically bring up the form and all undo all of the hiding. 
 
         public Form1()
         {
+            Debug.WriteLine("Debug Information-Product Starting: ");
             InitializeComponent();
-            if (true)
-            {
-                ShowInTaskbar = false;
-                this.FormBorderStyle = FormBorderStyle.None;
-                //this.ShowInTaskbar = false; // Change this when you're ready to be complete silent ninja
-                this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
-                this.Text = "THIS SHOULD BE HIDDEN";
-            }
+
+            Debug.WriteLine("THIS IS INITIALISING");
+
+            //if (HiddenGem)
+            //{
+                //this.ShowInTaskbar = false;
+                //this.FormBorderStyle = FormBorderStyle.None; // This needs @  protected override CreateParams CreateParams to make it hidden in taskbar too!
+                //this.WindowState = System.Windows.Forms.FormWindowState.Minimized; 
+                //this.Text = "Wifi Listener Settings"; //with above 2 lines text is tool title
+                //this.notifyIcon.Visible = true;
+            //}
+            Debug.WriteLine("THIS IS after form is hiden and tray icon. INITIALISING");
+
         }
+        
         protected override CreateParams CreateParams
         {
             get
@@ -28,7 +47,110 @@ namespace TestNetworkPrint
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            Debug.WriteLine("THIS HAPPENS UPON LOAD.... (please cheack saved files now");
+            String configSettings = LoadData();
+            if (configSettings != null)
+            {
+                // use LoadData to set settings and 
+                this.WindowState = System.Windows.Forms.FormWindowState.Normal;
 
+            } else
+            {
+                this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            }
+
+        }
+        // change save and load manager to be a seperate reusable class dummy
+
+
+        private string LoadData()
+        {
+            //test load config & if none exist, or the config is giberish return null 
+            Debug.WriteLine("Loading data ...");
+            Random random = new Random();
+            int n = random.Next(10);
+            if (n >= 5)
+            {
+                return "abcdef";
+            }
+            else
+                return null;
+            //throw new NotImplementedException();
+        }
+
+        private void ShowSettings()
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (checkBox4.Checked)
+                this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            //else do nothing.
+        }
+
+        private void closeExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //fakeexit
+            //realexit
+            if (debug)
+                Application.Exit();
+            else
+                fakeExit();
+        }
+
+        private void fakeExit()
+        {
+            // just hides the notifactionicon
+            this.notifyIcon1.Visible = false;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            //this.Text = "Wifi Listener Settings"; //with above 2 lines text is tool title
+            //throw new NotImplementedException();
+        }
+
+        private void openSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            //this.FormBorderStyle = FormBorderStyle.None; // This needs @  protected override CreateParams CreateParams to make it hidden in taskbar too!
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            //this.Text = "Wifi Listener Settings"; //with above 2 lines text is tool title
+            //this.notifyIcon.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fakeExit();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            //checkBox4.Checked = !checkBox4.Checked;
+            //OpenOnDoubleClick = checkBox4.Checked;
+
+            //This doesnt need to work (ie. nothing changes upon the change of the checkbox. )
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Hide Options")
+            {
+                this.Size = new Size(340, 190);
+                flowLayoutPanel1.Visible = false;
+                button1.Text = "Adv. Options";
+            }
+            else 
+            {
+                this.Size = new Size(340, 300);
+                button1.Text = "Hide Options";
+                flowLayoutPanel1.Visible = true;
+            }
+            
+            
         }
     }
 }
